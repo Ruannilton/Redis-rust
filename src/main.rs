e6c2746952dcbe4999c2db4e186c8cff3b2b510a
ruan.azevedo <ruannilton@gmail.com>
2024-05-16 01:58:02 -0300

    feat: handle multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
+
+const PONG_RESPONSE: &[u8] = "+PONG\r\n".as_bytes();
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for mut stream in listener.incoming() {
+    for stream in listener.incoming() {
         match stream {
-            Ok(ref mut stream) => {
-                let response = "+PONG\r\n".as_bytes();
-                let mut buf = [0; 512];
-                loop {
-                    let readed_count = stream.read(&mut buf).unwrap();
-                    if readed_count == 0 {
-                        break;
-                    }
-                    stream.write(response).unwrap();
-                }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client_connection(stream: TcpStream) {
+    let mut stream_copy = stream.try_clone().unwrap();
+    let mut client_request_buffer = [0; 512];
+
+    loop {
+        let readed_count = stream_copy.read(&mut client_request_buffer).unwrap();
+        if readed_count == 0 {
+            break;
+        }
+        stream_copy.write(PONG_RESPONSE).unwrap();
     }
 }
