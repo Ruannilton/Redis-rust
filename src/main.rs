a2bbf34bde4425cd4747831a7ca1f9a086fa21b8
ruan.azevedo <ruannilton@gmail.com>
2024-05-16 01:32:20 -0300

    fix: return multiple PONG response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -7,7 +10,14 @@ fn main() {
         match stream {
             Ok(ref mut stream) => {
                 let response = "+PONG\r\n".as_bytes();
-                stream.write(response).unwrap();
+                let mut buf = [0; 512];
+                loop {
+                    let readed_count = stream.read(&mut buf).unwrap();
+                    if readed_count == 0 {
+                        break;
+                    }
+                    stream.write(response).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
