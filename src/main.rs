c6683abe95d170ed7500d15eb89f8e95bc379fa4
ruan.azevedo <ruannilton@gmail.com>
2024-05-16 02:30:47 -0300

    feat: handle multiple connections with event loop

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-    thread,
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 const PONG_RESPONSE: &[u8] = "+PONG\r\n".as_bytes();
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client_connection(stream);
+    loop {
+        let accepted_result = listener.accept().await;
+
+        match accepted_result {
+            Ok((stream, _)) => {
+                tokio::spawn(async move {
+                    handle_client_connection(stream).await.unwrap();
                 });
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(_) => {
+                print!("Failed to handle request")
             }
         }
     }
 }
 
-fn handle_client_connection(stream: TcpStream) {
-    let mut stream_copy = stream.try_clone().unwrap();
+async fn handle_client_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut client_request_buffer = [0; 512];
 
     loop {
-        let readed_count = stream_copy.read(&mut client_request_buffer).unwrap();
+        let readed_count = stream.read(&mut client_request_buffer).await?;
         if readed_count == 0 {
             break;
         }
-        stream_copy.write(PONG_RESPONSE).unwrap();
+        stream.write(PONG_RESPONSE).await?;
     }
+    Ok(())
 }
