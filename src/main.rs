cb314021e51dc8712bf4e874c6416c7c6416c6f3
ruan.azevedo <ruannilton@gmail.com>
2024-07-23 18:20:42 -0300

    parse resp payload

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,54 @@
-pub mod resp_decoder;
-pub mod resp_encoder;
+pub mod resp_serialization;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
-const PONG_RESPONSE: &[u8] = "+PONG\r\n".as_bytes();
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    println!("Running...");
+
     loop {
         let accepted_result = listener.accept().await;
 
-        match accepted_result {
-            Ok((stream, _)) => {
-                tokio::spawn(async move {
-                    handle_client_connection(stream).await.unwrap();
-                });
-            }
-            Err(_) => {
-                print!("Failed to handle request")
-            }
+        if let Ok((stream, _)) = accepted_result {
+            tokio::spawn(async move {
+                handle_client_connection(stream).await.unwrap();
+            });
         }
     }
 }
 
+fn process_commands(buffer: &[u8]) -> String {
+    let command = resp_serialization::get_redis_command(buffer);
+
+    let response = match command {
+        resp_serialization::RedisCommand::Ping => String::from("PONG"),
+        resp_serialization::RedisCommand::Echo(s) => s,
+        _ => String::from("INVALID"),
+    };
+
+    let encoded = resp_serialization::encode_as_bulk_string(response);
+    encoded
+}
+
 async fn handle_client_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut client_request_buffer = Vec::new();
-
+    println!("here");
     loop {
-        let readed_count = stream.read_to_end(&mut client_request_buffer).await?;
+        let readed_count = stream.read_buf(&mut client_request_buffer).await.unwrap();
+
         if readed_count == 0 {
             break;
         }
 
+        println!("Readed: {}", readed_count);
+        println!("Payload: {:?}", client_request_buffer);
+
         let buffer = client_request_buffer.as_slice();
-        if let Some(command) = resp_decoder::BufferDecoder::decode(buffer) {
-            match command.command.as_str() {
-                "PING" => {
-                    println!("PING");
-                    stream.write(PONG_RESPONSE).await?;
-                }
-                "ECHO" => {
-                    if let Some(args) = command.args {
-                        let arg = args[0].clone();
-                        let resp = resp_encoder::resp_encode_bulk_string(arg);
-                        println!("ECHO");
-                        stream.write(resp.as_bytes()).await?;
-                    }
-                }
-                _ => {
-                    println!("Comand not recognized");
-                }
-            }
-        }
+
+        let response = process_commands(buffer);
+
+        stream.write(response.as_bytes()).await?;
     }
     Ok(())
 }
--- src/resp_encoder.rs
+++
@@ -1,3 +0,0 @@
-pub fn resp_encode_bulk_string(value: String) -> String {
-    format!("${}\r\n{}\r\n", value.len(), value)
-}
