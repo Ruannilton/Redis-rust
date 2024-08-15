749d30fbccda3b2898a1a70895b6101e7feb53d0
ruan.azevedo <ruannilton@gmail.com>
2024-08-15 17:16:30 -0300

    fix message response

--- src/redis/command/exec_command.rs
+++ src/redis/command/exec_command.rs
@@ -1,6 +1,6 @@
 use crate::{
     redis::{redis_app::RedisApp, redis_error::RedisError},
-    resp::resp_serializer::to_resp_string,
+    resp::resp_serializer::{to_err_string, to_resp_string},
 };
 
 use super::command_trait::Command;
@@ -15,6 +15,6 @@ impl ExecCommand {
 
 impl Command for ExecCommand {
     async fn execute(self, _: &RedisApp) -> Result<String, RedisError> {
-        Ok(to_resp_string("OK".to_owned()))
+        Ok(to_err_string("ERR EXEC without MULTI".to_owned()))
     }
 }
